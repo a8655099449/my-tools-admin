907d54eaaaf76c2eb79d609d5b815efee7f82797
kuangwei <865509949@qq.com>
2022-04-19 16:23:56 +0800

    feat: 提交测试

--- src/utils/use.ts
+++ src/utils/use.ts
@@ -1,25 +1,23 @@
-import { useLocation ,  } from "react-router-dom";
-
 export const useQuery = (): {
-  [key in string]: any;
+	[key in string]: any
 } => {
-  const query = {};
-  const search = decodeURI(location.href.split("?")[1]);
-  if (search) {
-    const qList = search.split("&");
-    qList.forEach((str) => {
-      if (str && str.includes("=")) {
-        // @ts-ignore
-        let [key, value]: [string, any] = str.split("=");
-        if (!isNaN(+value)) {
-          value = +value;
-        }
-        if (value == "true") value = true;
-        if (value == "false") value = false;
+	const query = {}
+	const search = decodeURI(location.href.split('?')[1])
+	if (search) {
+		const qList = search.split('&')
+		qList.forEach(str => {
+			if (str && str.includes('=')) {
+				// @ts-ignore
+				let [key, value]: [string, any] = str.split('=')
+				if (!isNaN(+value)) {
+					value = +value
+				}
+				if (value == 'true') value = true
+				if (value == 'false') value = false
 
-        query[key] = value;
-      }
-    });
-  }
-  return query;
-};
+				query[key] = value
+			}
+		})
+	}
+	return query
+}
