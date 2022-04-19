bc6b47918bf0f5b6dad2d5153da1ab5bcfdfe068
kuangwei <865509949@qq.com>
2022-04-19 16:45:22 +0800

    fix: testaaa

--- src/pages/rest-tool/to-type/index.tsx
+++ src/pages/rest-tool/to-type/index.tsx
@@ -1,75 +1,73 @@
-import { copy2Clipboard, parseObjectType } from "@/utils";
-import { Button, Card, Input } from "@arco-design/web-react";
-import React, { FC, ReactElement, useMemo, useState } from "react";
-import highlight from "highlight.js";
-import { IconCopy, IconCopyright } from "@arco-design/web-react/icon";
+import { copy2Clipboard, parseObjectType } from '@/utils'
+import { Button, Card, Input } from '@arco-design/web-react'
+import React, { FC, ReactElement, useMemo, useState } from 'react'
+import highlight from 'highlight.js'
+import { IconCopy, IconCopyright } from '@arco-design/web-react/icon'
 
-interface IProps {}
-
-const ToType: FC<IProps> = (): ReactElement => {
-  const [inputValue, setInputValue] = useState(
-    `{
+const ToType: FC = (): ReactElement => {
+	const [inputValue, setInputValue] = useState(
+		`{
   "id": 647,
   name:"55555",
   array:[],
   obj:{}
 }`
-  );
-  const output = useMemo(() => {
-    let _inputValue = inputValue.trim().replace(/;/g, ",").replace(/'/g, '"');
+	)
+	const output = useMemo(() => {
+		let _inputValue = inputValue.trim().replace(/;/g, ',').replace(/'/g, '"')
 
-    try {
-      const object: object = window.eval(`
+		try {
+			const object: object = window.eval(`
       new Object(${_inputValue})
-    `);
-      return parseObjectType(object);
-      console.log("👴parseObjectType>>>");
-    } catch (error) {
-      return ``;
-    }
-  }, [inputValue]);
+    `)
+			return parseObjectType(object)
+			console.log('👴parseObjectType>>>')
+		} catch (error) {
+			return ''
+		}
+	}, [inputValue])
 
-  const html = highlight.highlight(`type T = ${output}`, {
-    language: "ts",
-  }).value;
-  return (
-    <div>
-      <h2>请输入value</h2>
-      <Input.TextArea
-        style={{
-          height: 300,
-        }}
-        value={inputValue}
-        onChange={(e) => setInputValue(e)}
-      />
+	const html = highlight.highlight(`type T = ${output}`, {
+		language: 'ts',
+	}).value
+	return (
+		<div>
+			<h2>请输入value</h2>
+			<Input.TextArea
+				style={{
+					height: 300,
+				}}
+				value={inputValue}
+				onChange={e => setInputValue(e)}
+			/>
 
-      <h2>
-        type
-        <Button
-          onClick={() => {
-            copy2Clipboard(`type T = ${output}`);
-          }}
-          type="primary"
-          style={{
-            marginLeft: 10,
-          }}
-          icon={<IconCopy/>}
-        >
-          复制
-        </Button>
-      </h2>
-      <Card>
-        <div
-          dangerouslySetInnerHTML={{
-            __html: html,
-          }}
-          style={{
-            whiteSpace: "pre-wrap",
-          }}
-        />
-      </Card>
-    </div>
-  );
-};
+			<h2>
+				type
+				<Button
+					onClick={() => {
+						copy2Clipboard(`type T = ${output}`)
+					}}
+					type="primary"
+					style={{
+						marginLeft: 10,
+					}}
+					icon={<IconCopy />}
+				>
+					复制
+				</Button>
+			</h2>
+			<Card>
+				<div
+					dangerouslySetInnerHTML={{
+						__html: html,
+					}}
+					style={{
+						whiteSpace: 'pre-wrap',
+					}}
+				/>
+			</Card>
+		</div>
+	)
+}
 
-export default ToType;
+export default ToType
